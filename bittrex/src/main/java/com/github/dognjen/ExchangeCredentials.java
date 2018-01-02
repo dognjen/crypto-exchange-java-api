0fb0b727f37c76b0bad04be2eb941b38a2f4ab91
Dejan Ognjenović <dognjen@yandex.com>
2018-01-02 12:45:51 +0100

    Added ExchangeCredentials class

---
+++ bittrex/src/main/java/com/github/dognjen/ExchangeCredentials.java
@@ -0,0 +1,32 @@
+package com.github.dognjen;
+
+/**
+ * Created by Dejan on 02/01/2018.
+ */
+public class ExchangeCredentials {
+
+    public String apiKey;
+
+    public String apiSecret;
+
+    public ExchangeCredentials(String apiKey, String apiSecret) {
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+}
